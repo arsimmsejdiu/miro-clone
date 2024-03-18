6e00e63bcd4823033a4dedf00d4e8a7d8fb9fc49
arsimsejdiu <arsim.sejdiu@bik-air.com>
2024-03-18 04:44:50 +0100

    added board card and newe board button

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -3,6 +3,8 @@
 import { BoardListProps } from "@/interfaces/board-list-interface";
 import { useQuery } from "convex/react";
 import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./new-board-button";
+import { EmptyOr } from "@/assets";
 
 export const BoardList = ({
     orgId,
@@ -12,16 +14,16 @@ export const BoardList = ({
     //   orgId,
     //   ...query,
     // });
-    const data: boolean = true;
+    const data = undefined;
   
-    if (data === undefined) {
+    if (data !== undefined) {
       return (
         <div>
           <h2 className="text-3xl">
             {query.favorites ? "Favorite boards" : "Team boards"}
           </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
-            {/* <NewBoardButton orgId={orgId} disabled /> */}
+            <NewBoardButton orgId={orgId} disabled />
             <BoardCard.Skeleton />
             <BoardCard.Skeleton />
             <BoardCard.Skeleton />
@@ -49,10 +51,20 @@ export const BoardList = ({
           {query.favorites ? "Favorite boards" : "Team boards"}
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
-          {/* <NewBoardButton orgId={orgId} /> */}
+          <NewBoardButton orgId={orgId} />
           {/* {data?.map((board) => ( */}
             {/*TODO: BoardCard goes here */}
-            <p>Board Card List</p>
+            <BoardCard
+            key={1}
+            id={"1"}
+            title={"App Design"}
+            imageUrl={EmptyOr}
+            authorId={"12"}
+            authorName={"Arsim"}
+            createdAt={12}
+            orgId={"12"}
+            isFavorite={true}
+          />
             {/* ))} */}
         </div>
       </div>
